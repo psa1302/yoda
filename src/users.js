053b6086b69689bbe822e2778a08cea616217e3d
Puneet Saini <sainipuneet@live.in>
2020-03-28 01:25:29 +0530

    Initial commit

---
+++ src/users.js
@@ -0,0 +1,18 @@
+// users.js
+// Fake list of users to be used in the authentication
+var users = [
+  {
+    id: 1,
+    name: 'Vandana Manhas',
+    email: 'vandana_25@gmail.com',
+    password: 'pass123',
+  },
+  {
+    id: 2,
+    name: 'John',
+    email: 'John@mail.com',
+    password: 'pass123',
+  },
+]
+
+module.exports = users
